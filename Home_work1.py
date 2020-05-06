a0c8e867e42177cdbfdd2c3728e980c4da46585d
Ninja244 <trololo9795@gmail.com>
2020-05-06 18:34:35 +0300

    Vers1

---
+++ Home_work1.py
@@ -0,0 +1,8 @@
+import datetime
+print("Здраствуй")
+name = input("Стой куда ты? Ну ты и соня, как тебя зовут?: ")
+name = name.strip()
+print("Что это за имя такое? " + name.title() + " Ну хорошо")
+age = int(input("А скажи " +  name.title() + ", сколько тебе лет?: "))
+todayYear = datetime.datetime.now().year
+print("Ого так ты", + (todayYear - age), "года")
